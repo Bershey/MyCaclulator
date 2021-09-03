45328c4cca501cb2c8ecf1cce5b06f0776f18497
minminmikan <ff.remon.9123@gmail.com>
2021-09-04 00:18:55 +0900

    viewBindingの導入。

--- app/src/main/java/com/tomato/mycalculator/MainActivity.kt
+++ app/src/main/java/com/tomato/mycalculator/MainActivity.kt
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.tomato.mycalculator.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this,"ボタン",Toast.LENGTH_SHORT).show()
+
     }
 }
